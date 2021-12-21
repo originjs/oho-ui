710e31c9dc43d48ed5559beb23412429c3bb94c1
mullin <mjiao93@gmail.com>
2021-12-21 11:19:38 +0800

    feat(ui): add progress into oh-ui.

---
+++ entry/src/main/java/com/example/oho_ui/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.oho_ui;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
